9bc56f5238fa2b7791154ec4c8a48cfe43fc96e4
Gaurav Kumar <gk44035@gmail.com>
2025-03-20 23:32:28 +0530

    20 march

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+ 
+    images:{
+domains:['s3-alpha-sig.figma.com']
+    }
+  
+}
 
 export default nextConfig;
