435530903758414a76d8cf84246c7cb88bf1ea18
Gaurav Kumar <gk44035@gmail.com>
2025-04-15 23:15:53 +0530

    added apis in orders page

--- app/api/Order/index.ts
+++ app/api/Order/index.ts
@@ -1,51 +1,56 @@
 import { axiosInstance } from "@/app/utlis/axiosConfig/AxiosInstance";
 //create order
 export const createOrder = async (
-    product: { productId: string; quantity: number }[],
-    paymentId: string,
-    shippingAddress: {
-        fullName: string;
-        phone: string;
-        addressLine1: string;
-        city: string;
-        pincode: string;
-        state: string;
-    }
+  product: { productId: string; quantity: number }[],
+  paymentId: string,
+  shippingAddress: {
+    fullName: string;
+    phone: string;
+    addressLine1: string;
+    city: string;
+    pincode: string;
+    state: string;
+  }
 ) => {
-    try {
-        const response = await axiosInstance.post(
-            "/createOrder",
-            {
-                product,
-                paymentId,
-                shippingAddress,
-            }
-        );
-        return response.data;
-    } catch (error: any) {
-        console.error("Order creation failed:", error);
-        return { error: true, message: error?.response?.data?.message || "Failed to create order" };
-    }
+  try {
+    const response = await axiosInstance.post("/createOrder", {
+      product,
+      paymentId,
+      shippingAddress,
+    });
+    return response.data;
+  } catch (error: any) {
+    console.error("Order creation failed:", error);
+    return {
+      error: true,
+      message: error?.response?.data?.message || "Failed to create order",
+    };
+  }
 };
 
 // Get all orders for logged-in user
-export const getAllOrders = async () => {
-    try {
-        const response = await axiosInstance.get("/getAllOrders");
-        return response.data.data;
-    } catch (error: any) {
-        console.error("Failed to fetch orders:", error);
-        return [];
-    }
+export const getAllOrders = async (pageNo: number) => {
+  try {
+    const response = await axiosInstance.get("/getAllOrders", {
+      params: {
+        pageNo,
+      },
+    });
+    console.log(response, "res yah ape hu main");
+    return response.data;
+  } catch (error) {
+    console.log("Failed to fetch orders:", error);
+    return [];
+  }
 };
 
 //get order details
 export const getOrderDetails = async (orderId: string) => {
-    try {
-        const response = await axiosInstance.get(`/getOrderDetails/${orderId}`);
-        return response.data.data;
-    } catch (error: any) {
-        console.error("Failed to fetch order details:", error);
-        return null;
-    }
+  try {
+    const response = await axiosInstance.get(`/getOrderDetails/${orderId}`);
+    return response.data;
+  } catch (error: any) {
+    console.error("Failed to fetch order details:", error);
+    return null;
+  }
 };
--- app/customer/myorders/MyOrdersItem.tsx
+++ app/customer/myorders/MyOrdersItem.tsx
@@ -1,65 +1,101 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CancelOrderModel from "./CancelOrderModel";
 import Link from "next/link";
+import Pagination from "@/components/Pagination";
+import { getAllOrders } from "@/app/api/Order";
+import { dateFormate } from "@/app/utlis/dateFormate/dateFormating";
+interface OrderProduct {
+  _id: string;
+  productId: string;
+  quantity: number;
+  price: number;
+  productTitle: string;
+  images: string[];
+}
+interface Order {
+  _id: string;
+  userId: string;
+  createdAt: string; // or Date
+  orderStatus: string;
+  products: OrderProduct[];
+}
 
 const MyOrderItem = () => {
-  const products = [
-    {
-      title: "Large Mixing Bowl",
-      image: "/Images/bak1.jpg",
-      category: "Mixing",
-      in_stock: true,
-      review: 4.5,
-      status: "Delivered",
-      delivery: "Jan 2 2025",
-      orderId: "#34HJ85NVI4NJ4BFIEI5",
-    },
-    {
-      title: "Wooden Rolling Pin",
-      image: "/Images/bak2.jpg",
-      category: "Baking",
-      in_stock: false,
-      review: 1.3,
-      status: "Pending",
-      delivery: "Jan 2 2025",
-      orderId: "#34HJ85NVI4NJF9F8H3",
-    },
-    {
-      title: "Wooden Rolling Pin",
-      image: "/Images/bak2.jpg",
-      category: "Baking",
-      in_stock: false,
-      review: 3.3,
-      status: "Cancelled",
-      delivery: "Jan 2 2025",
-      orderId: "#34HJ85NVI4NZOAKXKZOA",
-    },
-  ];
+  const [currentPage, setCurrentPage] = useState(1);
+  // const [isLoading, setIsLoading] = useState(false);
+  const [totalPages, setTotalPages] = useState(1);
+  const [orders, setOrders] = useState([]);
+  function handleOnChange(pageNo: number) {
+    if (pageNo >= 1 && pageNo <= totalPages) {
+      setCurrentPage(pageNo);
+    }
+  }
+  // const products = [
+  //   {
+  //     title: "Large Mixing Bowl",
+  //     image: "/Images/bak1.jpg",
+  //     category: "Mixing",
+  //     in_stock: true,
+  //     review: 4.5,
+  //     status: "Delivered",
+  //     delivery: "Jan 2 2025",
+  //     orderId: "#34HJ85NVI4NJ4BFIEI5",
+  //   },
+  //   {
+  //     title: "Wooden Rolling Pin",
+  //     image: "/Images/bak2.jpg",
+  //     category: "Baking",
+  //     in_stock: false,
+  //     review: 1.3,
+  //     status: "Pending",
+  //     delivery: "Jan 2 2025",
+  //     orderId: "#34HJ85NVI4NJF9F8H3",
+  //   },
+  //   // {
+  //   //   title: "Wooden Rolling Pin",
+  //   //   image: "/Images/bak2.jpg",
+  //   //   category: "Baking",
+  //   //   in_stock: false,
+  //   //   review: 3.3,
+  //   //   status: "Cancelled",
+  //   //   delivery: "Jan 2 2025",
+  //   //   orderId: "#34HJ85NVI4NZOAKXKZOA",
+  //   // },
+  // ];
 
   const [cancle, setCancle] = useState<boolean>(false);
 
   const handleCloseModel = () => {
     setCancle((curr) => !curr);
   };
+  async function showOrdersData() {
+    const res = await getAllOrders(currentPage);
+    console.log(res, "res data orders ");
+    setOrders(res.data);
+    setTotalPages(res.totalPages);
+  }
+  useEffect(() => {
+    showOrdersData();
+  }, [currentPage]);
   return (
-    <div className="!cursor-pointer select-none relative">
+    <div className="!cursor-pointer select-none relative ">
       {cancle ? (
         <CancelOrderModel handleclose={handleCloseModel}></CancelOrderModel>
       ) : (
         ""
       )}
 
-      <p>{products.length} Items</p>
-      <div className="grid w-full grid-cols-1 py-2 gap-4 ">
-        {products.map((product, index) => (
+      <p>{orders?.length} Items</p>
+      <div className="grid w-full grid-cols-1 py-2 gap-4 min-h-90">
+        {orders?.map((product: Order, index: number) => (
           <div
             key={index}
             className="  w-full flex h-fit border-b py-[2rem] px-[2rem] border-gray-400"
           >
             <Image
-              src={product.image}
+              src={product?.products[0]?.images[0] || ""}
               alt=""
               height={400}
               width={400}
@@ -69,25 +105,26 @@ const MyOrderItem = () => {
               <div className="flex-1 gap-1">
                 <p className="whitespace-nowrap overflow-hidden">
                   {" "}
-                  Order Id : {product.orderId}
+                  Order Id : {product?._id}
                 </p>
                 <h1 className="text-[20px] font-bold  ">
-                  Gas Oven Three Deck Nine Tray
+                  {/* Gas Oven Three Deck Nine Tray */}
+                  {product?.products[0]?.productTitle}
                 </h1>
-                <p className="text-[20px] font-medium ">$234,344</p>
-                {product.status === "Delivered" ||
-                product.status === "Cancelled" ? (
-                  <Link href='myorders/34'>
-                  <button className="olive mt-3 text-white w-[120px] text-center py-2 rounded-lg">
-                    Order Details
-                  </button>
+                {/* <p className="text-[20px] font-medium ">$234,344</p> */}
+                {product?.orderStatus === "Delivered" ||
+                product?.orderStatus === "Cancelled" ? (
+                  <Link href={`myorders/${product?._id}`}>
+                    <button className="olive mt-3 text-white w-[120px] text-center py-2 rounded-lg">
+                      Order Details
+                    </button>
                   </Link>
                 ) : (
                   <div className="flex gap-4">
-                    <Link href='myorders/34'>
-                    <button className="olive mt-3 text-white w-[120px] text-center py-2 rounded-lg">
-                      Track Order
-                    </button>
+                    <Link href={`myorders/${product?._id}`}>
+                      <button className="olive mt-3 text-white w-[120px] text-center py-2 rounded-lg">
+                        Track Order
+                      </button>
                     </Link>
                     <button
                       onClick={handleCloseModel}
@@ -102,30 +139,37 @@ const MyOrderItem = () => {
                 Status
                 <p
                   className={`${
-                    product.status == "Delivered"
+                    product?.orderStatus == "Delivered"
                       ? "text-green-500"
-                      : product.status === "Cancelled"
+                      : product?.orderStatus === "Cancelled"
                       ? "text-red-500"
                       : "text-yellow-500"
                   }`}
                 >
                   {" "}
-                  {product.status}
+                  {product?.orderStatus}
                 </p>
               </div>
               <div className="flex-1 flex items-center text-[20px] flex-col gap-2 font-bold ">
-                Dilevery Excepted By
-                {product.status === "Delivered" ||
-                product.status === "Cancelled" ? (
+                Order On
+                {/* {product?.orderStatus === "Delivered" ||
+                product?.orderStatus === "Cancelled" ? (
                   ""
-                ) : (
-                  <p className="font-normal">{product.delivery}</p>
-                )}
+                ) : ( */}
+                <p className="font-normal">{dateFormate(product.createdAt)}</p>
+                {/* )} */}
               </div>
             </div>
           </div>
         ))}
       </div>
+
+      <Pagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        handleOnChange={handleOnChange}
+        user="customer"
+      ></Pagination>
     </div>
   );
 };
