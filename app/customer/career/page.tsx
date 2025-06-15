8737640085613eff028f423f1092052fe8e04cf3
Gaurav Kumar <gk44035@gmail.com>
2025-06-15 16:00:32 +0530

    career page responsive

--- app/customer/career/page.tsx
+++ app/customer/career/page.tsx
@@ -18,7 +18,7 @@ const Career = () => {
     description:
       "Frost Master Private Limited is committed to providing career opportunities that allow employees to grow and succeed. As a leader in the refrigeration and air conditioning industry, Frost Master offers a dynamic and challenging work environment that values innovation and teamwork. Employees are provided with ongoing training and development opportunities to enhance their skills and knowledge. Frost Master also recognizes and rewards high performers, providing opportunities for advancement and career growth. Join the Frost Master team to be a part of a company that values its employees and supports their career aspirations. Visit their website to learn more about current job openings and career opportunities.",
     image: `${process.env.NEXT_PUBLIC_CDNURL}Images/CareerPage/img5.jpg`,
-    align: "flex-col lg:flex-row ",
+    align: "flex-col md:flex-row ",
     justify: "justify-start",
     border: "border-r-4 border-b-4 border-[#35736E]",
   };
@@ -26,7 +26,7 @@ const Career = () => {
   const [formdata, setFormdata] = useState<FormData>({
     name: "",
     email: "",
-    phone: 0,
+    phone: 91,
     message: "",
   });
   //   const [file,setFile]=useState<File>()
@@ -56,14 +56,14 @@ const Career = () => {
             height={400}
             width={400}
             data-aos="fade-right"
-            className=" h-[40dvh] lg:w-[50%] w-full  md:h-[70dvh] rounded-md shadow-lg"
+            className=" h-[40dvh] sm:w-[50%] w-full  md:h-[70dvh] rounded-md shadow-lg"
           ></Image>
-          <div data-aos="fade-left" className="flex flex-1 px-[2rem] flex-col">
-            <h2 className="font-bold text-[40px] w-fit border-b-4 border-[#35736E] my-[1rem] text-[#35736E]">
+          <div data-aos="fade-left"  className="flex flex-1  md:px-[2rem] flex-col">
+            <h2 className="font-bold leading-[40px] md:text-[40px] text-[35px] w-fit border-b-4 border-[#35736E] my-[1rem] text-[#35736E]">
               {" "}
               {features.title}
             </h2>
-            <div className="flex flex-col ">
+            <div className="flex flex-col  md:h-[45dvh] overflow-y-scroll ">
               <div className=" text-[18px] text-slate-700  font-normal flex-row">
                 <p>{features.description}</p>
                 <br></br>
@@ -121,9 +121,16 @@ const Career = () => {
                     className="border border-[#35736E] text-[#35736E] outline-none w-full py-2 px-2 rounded-md "
                   />
                   <input
-                    type="number"
+                    type="text"
                     name="phone"
-                    onChange={handleChange}
+                    maxLength={10}
+                    onChange={(e) => {
+                      const value = e.target.value;
+                      if (/^\d*$/.test(value)) {
+                        handleChange(e); // only call your handler if the input is digits only
+                      }
+                    }}
+                    pattern="[0-9]{10}"
                     placeholder="Enter Your Phone"
                     className="border border-[#35736E] text-[#35736E] outline-none w-full py-2 px-2 rounded-md "
                   />
