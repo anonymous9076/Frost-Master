524297b68ec8eb970abcb7cfa09c79a78511e941
Gaurav Kumar <gk44035@gmail.com>
2025-06-26 23:50:51 +0530

    new push

--- app/customer/[section]/[title]/page.tsx
+++ app/customer/[section]/[title]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import rawData from "../../../../SpecificData.json";
-import { SectionData, CommonData } from "../../../../SpecificDataTypes";
+import {CommonData } from "../../../../SpecificDataTypes";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
