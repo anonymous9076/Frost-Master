c8b3124d160e14048f47fed56ab9c75a58291724
Gaurav Kumar <gk44035@gmail.com>
2025-07-03 23:30:51 +0530

    why frost master

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = ({ active }: navprops) => {
         "Experiences Shared by Our Clients",
         "Trusted by thousands Businesses",
         "What Makes us prominent players",
-        "Why frostmaster.in?",
+        "Why frost master",
       ],
       img: "Images/PremiumKitchenEquipments/img4.jpg",
     },
