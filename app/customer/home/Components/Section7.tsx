e6f11dbabdc9971871478b42b573ff5df8efc8d5
Gaurav Kumar <gk44035@gmail.com>
2025-07-06 16:47:49 +0530

    add new cities

--- app/customer/home/Components/Section7.tsx
+++ app/customer/home/Components/Section7.tsx
@@ -15,6 +15,14 @@ const obj = [
     label: "Delhi (Head Office)",
     image: `${process.env.NEXT_PUBLIC_CDNURL}Images/City/Delhi.jpg`,
   },
+  {
+    label: "Gurgaon, Haryana",
+    image: `/gurgaon.jpg`,
+  },
+  {
+    label: "Muzaffarpur, Bihar",
+    image: `/Muzaffarpur.jpg`,
+  },
 ];
 
   return (
@@ -22,22 +30,22 @@ const obj = [
       <h1 data-aos="fade-up" className="w-full responsive-heading py-[3rem] text-center font-bold ">
         Connecting Cities, Delivering Quality
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 px-[8%]">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2 px-[5%]">
         {obj.map((item, index) => (
           <div
           
            data-aos="fade-up"
-            data-aos-delay={`${(index + 2) * 100}`}
-            key={index} className="  !max-h-[500px] min-w-[300px]">
-            <div className="h-fit  w-full flex flex-col justify-between items-center ">
+            data-aos-delay={`${(index + 1) * 100}`}
+            key={index} className=" overflow-hidden rounded-xl">
+            <div className="h-fit  w-full flex group flex-col !overflow-hidden transition-all duration-500 ease-linear relative justify-between items-center ">
               <Image
                 src={item.image}
                 alt=""
                 height={400}
                 width={400}
-                className=" aspect-[3/3.5] object-cover rounded-xl shadow-md"
+                className=" aspect-[3/3.5] group-hover:scale-120 group-focus:scale-120  transition-all duration-300 ease-linear object-cover rounded-xl shadow-md"
               ></Image>
-              <p className="text-[25px] text-gray-700 !font-semibold text-center">{item.label}</p>
+              <p className="text-[20px] h-full w-full bg-black/20 flex items-center rounded-xl justify-center text-white absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2  !font-semibold text-center">{item.label}</p>
             </div>
           </div>
         ))}
